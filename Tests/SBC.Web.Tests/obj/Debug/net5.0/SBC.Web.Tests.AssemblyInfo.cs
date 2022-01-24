9d98179b97308c605fb1d06d95121e272f096173
knaevKMK <knev@mail.bg>
2022-01-24 20:53:31 +0200

    edit

--- Tests/SBC.Web.Tests/obj/Debug/net5.0/SBC.Web.Tests.AssemblyInfo.cs
+++ Tests/SBC.Web.Tests/obj/Debug/net5.0/SBC.Web.Tests.AssemblyInfo.cs
@@ -12,8 +12,8 @@ using System;
 using System.Reflection;
 
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap4")]
-[assembly: Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryContentRootAttribute("SBC.Data, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "C:\\GITHUB\\SBC\\Data\\SBC.Data", "SBC.Data.csproj", "0")]
-[assembly: Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryContentRootAttribute("SBC.Web, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "C:\\GITHUB\\SBC\\Web\\SBC.Web", "SBC.Web.csproj", "0")]
+[assembly: Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryContentRootAttribute("SBC.Data, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "C:\\Users\\master\\Desktop\\GitHub\\BootCamp.Project.SBC\\Data\\SBC.Data", "SBC.Data.csproj", "0")]
+[assembly: Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryContentRootAttribute("SBC.Web, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "C:\\Users\\master\\Desktop\\GitHub\\BootCamp.Project.SBC\\Web\\SBC.Web", "SBC.Web.csproj", "0")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("SBC.Web.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
