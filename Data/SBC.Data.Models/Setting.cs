c226bea2e87b75caf35a7ea7ac187195757cb99a
knaevKMK <71128514+knaevKMK@users.noreply.github.com>
2022-01-24 20:34:46 +0200

    create app

---
+++ Data/SBC.Data.Models/Setting.cs
@@ -0,0 +1,11 @@
+﻿namespace SBC.Data.Models
+{
+    using SBC.Data.Common.Models;
+
+    public class Setting : BaseDeletableModel<int>
+    {
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
