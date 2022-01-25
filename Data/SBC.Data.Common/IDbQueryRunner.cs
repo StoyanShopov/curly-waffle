c9dc3fb556b19fc220d3c4236a722f29a3aeb0ea
stosho <stosho@cobuilder.no>
2022-01-25 18:09:21 +0200

    introduce new project

---
+++ Data/SBC.Data.Common/IDbQueryRunner.cs
@@ -0,0 +1,10 @@
+﻿namespace SBC.Data.Common
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public interface IDbQueryRunner : IDisposable
+    {
+        Task RunQueryAsync(string query, params object[] parameters);
+    }
+}
