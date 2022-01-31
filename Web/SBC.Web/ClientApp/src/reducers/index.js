15a32f295c2049985017e17a45e401e115bfc033
WolfieSquat <62526576+WolfieSquat@users.noreply.github.com>
2022-01-31 20:31:18 +0200

    test

---
+++ Web/SBC.Web/ClientApp/src/reducers/index.js
@@ -0,0 +1,13 @@
+﻿import { } from 'redux';
+
+import { authentication } from './authentication.reducer';
+import { users } from './users.reducer';
+import { alert } from './alert.reducer';
+
+const rootReducer = combineReducers({
+    authentication,
+    users,
+    alert
+});
+
+export default rootReducer;
