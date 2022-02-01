681c26c2ab4c260e2de286686ce936ac71b926af
VentsislavAleksandrov <69898297+VentsislavAleksandrov@users.noreply.github.com>
2022-02-01 16:22:19 +0200

    Update App.js

--- Web/SBC.Web/ClientApp/src/App.js
+++ Web/SBC.Web/ClientApp/src/App.js
@@ -1,8 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
 import Homepage from './components/Homepage/Homepage';
+
 function App() {
   return (
     <div className="App"> 
-      <Homepage/>
+      <Routes>
+        <Route path="/" element={<Homepage/>}/>
+      </Routes>    
     </div>
   );
 }
