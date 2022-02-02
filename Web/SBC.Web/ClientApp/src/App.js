5468d222f40ca6a94950f06e613701e42bdd00da
WolfieSquat <62526576+WolfieSquat@users.noreply.github.com>
2022-02-02 02:31:00 +0200

    Squashed commit of the following:
    commit 437dd6ae80adad5a1e82c40c809a6ac3d954716d
    Author: Ivan Nikolov <nikolov.iv@gmail.com>
    Date:   Tue Feb 1 07:14:32 2022 +0200
    
        work done
    
    commit f22f63d733eec9a1dbaa56f49755eeee9b0cf081
    Author: Ivan Nikolov <nikolov.iv@gmail.com>
    Date:   Mon Jan 31 10:36:06 2022 +0200
    
        Add login component - work in progress

--- Web/SBC.Web/ClientApp/src/App.js
+++ Web/SBC.Web/ClientApp/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import LoginAsEmployee from "./pages/LoginAsEmployee";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="loginAsEmployee" element={<LoginAsEmployee />} />
+      </Routes>
     </div>
   );
 }
---
+++ Web/SBC.Web/ClientApp/src/pages/LoginAsEmployee.js
@@ -0,0 +1,57 @@
+﻿import React from "react";
+import styles from "./LoginAsEmployee.module.css";
+import arrow from "./../images/Group 5.svg";
+import backgnd from "./../images/Path 9.svg";
+import woman from "./../images/Group 17.svg";
+import eye from "./../images/Icon - Visibility - Filled.svg";
+
+export default function LoginAsEmployee(prop) {
+  const link = "Забравена парола?";
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <img src={backgnd} alt="" className={styles.backgnd} />
+        <img src={woman} alt="" className={styles.woman} />
+      </div>
+      <div className={styles.right}>
+        <div className={styles.formContainer}>
+          <div className={styles.arrowContainer}>
+            <img src={arrow} className={styles.arrow} alt="" />
+          </div>
+          <h1>Welcome back!</h1>
+          <h5>Please login to your account</h5>
+          <form action="">
+            <div className={styles.inputcontainer}>
+              <input
+                type="text"
+                className={`${styles.input} ${styles.inputuser}`}
+                name="name"
+                required="required"
+                id="name"
+                placeholder="Email Address*"
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                className={`${styles.input} ${styles.inputpass}`}
+                placeholder={"Password*"}
+              />
+            </div>
+            <div className={styles.checkcontainer}>
+              <label htmlFor="">
+                <input type="checkbox" name="" />
+                Запомни ме
+              </label>
+              <a href="/">{link}</a>
+            </div>
+            <div className={styles.btncontainer}>
+              <input type="button" value="Login" />
+              <input type="button" value="SignUp" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
