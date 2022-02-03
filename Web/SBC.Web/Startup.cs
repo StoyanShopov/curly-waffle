c2ea2d638df39ec0b0d9036642168a7d8727b9f5
Simeon Yankov <Simeon.n.qnkov@gmail.com>
2022-02-03 14:57:47 +0200

    Async with dev branch

--- Web/SBC.Web/Startup.cs
+++ Web/SBC.Web/Startup.cs
@@ -7,8 +7,6 @@
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Http;
-    using Microsoft.AspNetCore.Mvc;
-
     using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
@@ -16,7 +14,6 @@
     using Microsoft.Extensions.Hosting;
     using Microsoft.IdentityModel.Tokens;
     using Microsoft.OpenApi.Models;
-
     using SBC.Data;
     using SBC.Data.Common;
     using SBC.Data.Common.Repositories;
