d90a82f7b15f55fd358dd8fffce93cb4f7bcc938
Yovko Garkov <iovko1993@gmail.com>
2022-02-04 16:39:01 +0200

    Add course view model

---
+++ Web/SBC.Web.ViewModels/Course/CourseViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace SBC.Web.ViewModels.Course
+{
+    using SBC.Data.Models;
+    using SBC.Services.Mapping;
+
+    public class CourseViewModel : IMapFrom<Course>
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal PricePerPerson { get; set; }
+
+        public string VideoUrl { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int LanguageId { get; set; }
+
+        public int CoachId { get; set; }
+    }
+}
