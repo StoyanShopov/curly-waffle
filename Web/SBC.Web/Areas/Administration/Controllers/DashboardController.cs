fe52826ce94628a218a5eff5502084d7c4324e0c
knaevKMK <71128514+knaevKMK@users.noreply.github.com>
2022-02-07 20:01:07 +0200

    edit cotroller

--- Web/SBC.Web/Areas/Administration/Controllers/DashboardController.cs
+++ Web/SBC.Web/Areas/Administration/Controllers/DashboardController.cs
@@ -2,56 +2,19 @@
 {
     using System.Threading.Tasks;
 
-    using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
-    using SBC.Common;
-    using SBC.Data.Common.Repositories;
-    using SBC.Data.Models;
-    using SBC.Services.Data.Admin.Models;
-    using SBC.Services.Data.Infrastructures;
+    using SBC.Services.Data.Admin.Contracts;
 
-    public class ProfileController : AdministrationController
+    public class DashboardController : AdministrationController
     {
-        private readonly IDeletableEntityRepository<ApplicationUser> applicationUser;
-        private readonly UserManager<ApplicationUser> userManager;
-        // TODO: ADD SERVICE
         private readonly IDasboardService profileService;
 
-
-        public ProfileController(
-            IDeletableEntityRepository<ApplicationUser> applicationUser,
-            UserManager<ApplicationUser> userManager, IDasboardService profileService)
+        public DashboardController(IDasboardService profileService)
         {
-            this.applicationUser = applicationUser;
-            this.userManager = userManager;
             this.profileService = profileService;
         }
 
         [HttpGet]
-        [Route("index")]
-        public async Task<ActionResult> GetDasboard() => this.GenericResponse(await this.profileService.GetDashboard());
-
-        [HttpPut]
-        [Route(nameof(Edit))]
-
-        public async Task<ActionResult> Edit(EditProfileServiceModel model)
-        {
-            var userId = this.User.Id();
-
-            var user = await this.userManager.FindByIdAsync(userId);
-
-            user.Email = model.Email;
-            user.FirstName = model.Fullname;
-            user.ProfileSummary = model.ProfileSummary;
-            // user.PhotoUrl = model.PhotoUrl;
-
-            var result = await this.userManager.UpdateAsync(user);
-
-            return this.GenericResponse(new ResultModel(
-            new
-            {
-                Result = result,
-            }));
-        }
+        public async Task<ActionResult> Dasboard() => this.GenericResponse(await this.profileService.GetDashboard());
     }
 }
---
+++ Web/SBC.Web/Areas/Administration/Controllers/ProfileController.cs
@@ -0,0 +1,49 @@
+﻿namespace SBC.Web.Areas.Administration.Controllers
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using SBC.Common;
+    using SBC.Data.Common.Repositories;
+    using SBC.Data.Models;
+    using SBC.Services.Data.Admin.Models;
+    using SBC.Services.Data.Infrastructures;
+
+    public class ProfileController : AdministrationController
+    {
+        private readonly IDeletableEntityRepository<ApplicationUser> applicationUser;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        // TODO: ADD SERVICE
+        public ProfileController(
+            IDeletableEntityRepository<ApplicationUser> applicationUser,
+            UserManager<ApplicationUser> userManager)
+        {
+            this.applicationUser = applicationUser;
+            this.userManager = userManager;
+        }
+
+        [HttpPut]
+        [Route(nameof(Edit))]
+
+        public async Task<ActionResult> Edit(EditProfileServiceModel model)
+        {
+            var userId = this.User.Id();
+
+            var user = await this.userManager.FindByIdAsync(userId);
+
+            user.Email = model.Email;
+            user.FirstName = model.Fullname;
+            user.ProfileSummary = model.ProfileSummary;
+            // user.PhotoUrl = model.PhotoUrl;
+            var result = await this.userManager.UpdateAsync(user);
+
+            return this.GenericResponse(new ResultModel(
+            new
+            {
+                Result = result,
+            }));
+        }
+    }
+}
