2e9a9d49d327daa403829c35b89c55e2e624ec2d
Yovko Garkov <iovko1993@gmail.com>
2022-02-10 15:55:21 +0200

    Create new folder

--- Web/SBC.Web/ClientApp/src/App.js
+++ Web/SBC.Web/ClientApp/src/App.js
@@ -6,15 +6,12 @@ import './App.css';
 import './components/Admin/Course/DeleteCourse/DeleteCourse.js';
 import DeleteCourse from './components/Admin/Course/DeleteCourse/DeleteCourse.js';
 import CreateCourse from './components/Admin/Course/CreateCourse/CreateCourse.js';
+import AllCourses from "./components/Admin/Course/AllCourses/AllCourses";
 
 function App() {
   return (
     <div className="App">
-      {/* <CreateLecture/>
-      <EditLecture/>
-      */}
-      {/* <DeleteCourse /> */}
-      <CreateCourse />
+      <AllCourses />
     </div>
   );
 }
