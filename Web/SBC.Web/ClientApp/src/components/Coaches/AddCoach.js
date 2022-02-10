ac55c4ba2298872ef9c6f9819001ebe2dfb333e8
Daniel Hristov <danihristov1@abv.bg>
2022-02-10 20:58:46 +0200

    work in progress

--- Web/SBC.Web/ClientApp/src/components/Coaches/AddCoach.js
+++ Web/SBC.Web/ClientApp/src/components/Coaches/AddCoach.js
@@ -1,9 +1,47 @@
+import { useState,useEffect } from 'react';
+
 import styles from './AddCoach.module.css';
 
-const AddCoach = () =>
-{
-    return(
-        <body>
+
+const AddCoach = () => {
+
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('')
+    const [price, setPrice] = useState(0);
+    const [description, setDescription] = useState('');
+    const [callendlyUrl, setCalendlyUrl] = useState('');
+    const [file, setFile] = useState('');
+
+    const onChangeLastName = (e) =>{
+        setLastName(e.target.value);
+    }
+
+    const onChangeFirstName = (e) =>{
+        setFirstName(e.target.value);
+    }
+
+    const onChangeCallendlyUrl = (e) =>{
+        setCalendlyUrl(e.target.value);
+    }
+
+    const onChangeFile = (e) =>{
+        setFile(e.target.file[0]);
+    }
+
+    const onChangeDescription = (e) =>{
+        setDescription(e.target.value);
+    }
+
+    const onChangePrice = (e) =>{
+        setPrice(e.target.value)
+    }
+
+    const onSubmitAddCoach=(e) =>{
+        e.preventDefault()
+    }
+
+    return (
+        <body className={styles.body}>
             <section>
                 <div className={styles.modal}>
                     <div className={styles.modalHead}>
@@ -13,42 +51,87 @@ const AddCoach = () =>
                         <h2 className={styles.modalTitle}>Add Coaches</h2>
                     </div>
                     <div className={styles.modalContented}>
-                        <form className={styles.formMain}>
+                        <form 
+                        className={styles.formMain}
+                        // onSubmit={(e) =>{onSubmitAddCoach}}
+                        >
                             <div className={styles.formGroup, styles.field}>
-                                <input type="input" className={styles.formField} placeholder="First Name *" name="firstname" id="firstName"
+                                <input 
+                                type="text" 
+                                className={styles.formField} 
+                                placeholder="First Name*" 
+                                id="firstName"
+                                value={firstName}
+                                onChange={onChangeFirstName}
                                     required />
-                                <label for="firstname" className={styles.formLabel}></label>
+                                <label htmlFor="firstname" className={styles.formLabel}></label>
                             </div>
 
                             <div className={styles.formGroup, styles.field}>
-                                <input type="input" className={styles.formField} placeholder="Last Name *" name="lastname" id="lastname"
+                                <input 
+                                type="text" 
+                                className={styles.formField} 
+                                placeholder="Last Name*" 
+                                id="lastname"
+                                value={lastName}
+                                onChange={onChangeLastName}
                                     required />
-                                <label for="lastname" className={styles.formLabel}></label>
                             </div>
 
                             <div className={styles.formGroup, styles.field}>
-                                <input type="input" className={styles.formField} placeholder="Price *" name="price" id="price" required />
-                                <label for="price" className={styles.formLabel}></label>
-                            </div>
-
-                            <div className={styles.formGroup, styles.field}>
-                                <input type="input" className={styles.formField} placeholder="Description *" name="description"
-                                    id="description" required />
-                                <label for="description" className={styles.formLabel}></label>
-                            </div>
-
-                            <div className={styles.formGroup, styles.field}>
-                                <input type="input" className={styles.formField} placeholder="Callendly-URL *" name="calendly" id="calendly"
+                                <input 
+                                type="text" 
+                                className={styles.formField} 
+                                placeholder="Price*" 
+                                id="price" 
+                                onChange={onChangePrice}
                                     required />
-                                <label for="calendly" className={styles.formLabel}></label>
+                            </div>
+
+                            <div className={styles.formGroup, styles.field}>
+                                <input 
+                                type="text" 
+                                className={styles.formField} 
+                                placeholder="Description*" 
+                                id="description" 
+                                value={description}
+                                onChange={onChangeDescription}
+                                    required />
+                            </div>
+
+                            <div className={styles.formGroup, styles.field}>
+                                <input 
+                                type="text" 
+                                className={styles.formField} 
+                                placeholder="Callendly-URL*" 
+                                id="calendly"
+                                value={callendlyUrl}
+                                onChange={onChangeCallendlyUrl}
+                                    required />
+                            </div>
+
+                            <div className={styles.formGroup, styles.field}>
+                                <input 
+                                type="file" 
+                                className={styles.formField} 
+                                placeholder="file*" 
+                                id="file"
+                                value={file}
+                                onChange={onChangeFile}
+                                     required />
+                            </div>
+
+                            <button className={styles.addAnotherCoachBtn}>
+                                + Create another coach
+                            </button>
+
+                            <div className={styles.modalFooter}>
+                                <div className={styles.submitBox}>
+                                    <button className={styles.modalBtnCancel} type="submit">Cancel</button>
+                                    <button className={styles.modalBtnSave} type="submit">Save</button>
+                                </div>
                             </div>
                         </form>
-                    </div>
-                    <div className={styles.modalFooter}>
-                        <div className={styles.submitBox}>
-                            <button className={styles.modalBtnCancel} type="submit">Cancel</button>
-                            <button className={styles.modalBtnSave} type="submit">Save</button>
-                        </div>
                     </div>
                 </div>
             </section>
