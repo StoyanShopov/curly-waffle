8f2afdd616b49592c2e9a7f22e10cc191c7f8fa9
VentsislavAleksandrov <69898297+VentsislavAleksandrov@users.noreply.github.com>
2022-02-14 20:29:48 +0200

    fix for create lecture modal

--- Web/SBC.Web/ClientApp/src/components/Admin/Lecture/TestHomePage.js
+++ Web/SBC.Web/ClientApp/src/components/Admin/Lecture/TestHomePage.js
@@ -7,7 +7,7 @@ export default function Home() {
     const [openAddFormModal, setOpenAddFormModal] = useState(false);
 
     return (
-        <div>
+        <div >
             <h1>Open Modal</h1>
             <button className={css.openModalBtn} onClick = {() => {setOpenAddFormModal(true)}}>Open</button>
             {openAddFormModal && <CreateLecture closeModal={setOpenAddFormModal}/>}
