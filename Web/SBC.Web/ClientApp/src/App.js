10984b41d2ee6afd7109097afd722dc8c7246198
VentsislavAleksandrov <69898297+VentsislavAleksandrov@users.noreply.github.com>
2022-02-14 18:22:12 +0200

    Fix for createlectureModal

--- Web/SBC.Web/ClientApp/src/App.js
+++ Web/SBC.Web/ClientApp/src/App.js
@@ -2,11 +2,12 @@ import EditLecture from "./components/Admin/Lecture/EditLecture/EditLecture"
 import CreateLecture from "./components/Admin/Lecture/CreateLecture/CreateLecture";
 import DeleteLecture from "./components/Admin/Lecture/DeleteLecture/DeleteLecture";
 import CourseDetails from "./components/Admin/Course/CourseDetails/CourseDetails";
+import TestHomePage from "./components/Admin/Lecture/TestHomePage"
 
 function App() {
   return (
     <div className="App">
-       <CourseDetails/>
+       <TestHomePage />
     </div>
   );
 }
---
+++ Web/SBC.Web/ClientApp/src/components/Admin/Lecture/TestHomePage.js
@@ -0,0 +1,16 @@
+import css from "./TestHomePage.module.css"
+import { useState } from "react"
+import CreateLecture from "./CreateLecture/CreateLecture";
+
+export default function Home() {
+    
+    const [openAddFormModal, setOpenAddFormModal] = useState(false);
+
+    return (
+        <div>
+            <h1>Open Modal</h1>
+            <button className={css.openModalBtn} onClick = {() => {setOpenAddFormModal(true)}}>Open</button>
+            {openAddFormModal && <CreateLecture closeModal={setOpenAddFormModal}/>}
+        </div>
+    )
+}
