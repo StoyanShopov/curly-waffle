62fd72e60d75cce364225b091e1cc09d86d7f94c
Aleksandar Hristov <64472282+AleksandarHristovGithub@users.noreply.github.com>
2022-02-15 13:10:55 +0200

    Changed css to module.css

--- Web/SBC.Web/ClientApp/src/components/SignUpAsBusinessOwner/Signup.js
+++ Web/SBC.Web/ClientApp/src/components/SignUpAsBusinessOwner/Signup.js
@@ -5,19 +5,19 @@ import Logo from "../assets/Group5.png"
 import SignupFigure from "../assets/Path9.png";
 import Man from "../assets/Group52.png";
 
-import css from "./Signup.css";
+import css from "./Signup.module.css";
 
 function Signup() {
     return (
-        <section> <div className="picDiv">
-        <img src={SignupFigure} className="signupFigure" />
-        <img src={Man} className="Man"/>
+        <section> <div className={css.picDiv}>
+        <img src={SignupFigure} className={css.signupFigure} />
+        <img src={Man} className={css.Man}/>
     </div>
-    <div className="ButtonAndText">
+    <div className={css.ButtonAndText}>
     <ul>
-        <li className="Logo"><img src={Logo}/></li>
-        <Link to="/Signup"><li className="Button"><button>SignUp as Business Owner</button></li></Link>
-        <li className="TextOnBottom"><p className="have">Already have an account?</p> <Link to="/Login"><p className="login">LogIn here.</p></Link></li>
+        <li className={css.Logo}><img src={Logo}/></li>
+        <Link to="/Signup"><li className={css.Button}><button>SignUp as Business Owner</button></li></Link>
+        <li className={css.TextOnBotto}><p className={css.have}>Already have an account?</p> <Link to="/Login"><p className={css.login}>LogIn here.</p></Link></li>
     </ul>
     </div></section>
        
