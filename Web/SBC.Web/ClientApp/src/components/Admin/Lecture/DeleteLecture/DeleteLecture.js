2aad325d107576e0616c43756ed94d8c545e9c67
VentsislavAleksandrov <69898297+VentsislavAleksandrov@users.noreply.github.com>
2022-02-15 10:48:41 +0200

    Fix for Edit and Delete Modals

--- Web/SBC.Web/ClientApp/src/components/Admin/Lecture/DeleteLecture/DeleteLecture.js
+++ Web/SBC.Web/ClientApp/src/components/Admin/Lecture/DeleteLecture/DeleteLecture.js
@@ -1,14 +1,14 @@
 import css from "./DeleteLecture.module.css"
 
-export default function DeleteLecture() {
+export default function DeleteLecture({closeDeleteModal}) {
     return (
         <section className={css.section}>
             <div className={css.topDiv}>
-                <span className={css.close}>&times;</span>
-                <p className={css.p}>Are you sure you want to remove this course?</p>
+                <button className={css.btnClose} onClick = {() => {closeDeleteModal(false)}}>X</button>
+                <p className={css.p}>Are you sure you want to remove this lecture?</p>
             </div>
             <div className={css.bottomDiv}>
-            <button className={css.btnCancel}>Cancel</button>
+            <button className={css.btnCancel} onClick = {() => {closeDeleteModal(false)}}>Cancel</button>
             <button className={css.btnRemove}>Remove</button>
             </div>
         </section>
--- Web/SBC.Web/ClientApp/src/components/Admin/Lecture/EditLecture/EditLecture.js
+++ Web/SBC.Web/ClientApp/src/components/Admin/Lecture/EditLecture/EditLecture.js
@@ -1,17 +1,21 @@
 import css from "./EditLecture.module.css"
-export default function EditLecture(){
-    return(
+export default function EditLecture({ closeEditModal }) {
+    return (
         <section className={css.section}>
-            <form>
-                <div>
-                    <p className={css.p}>Edit Lecture</p>
-                </div>
-                <div>
-                    <input className={css.imput} name = "Name" placeholder="Name*"></input>
-                    <input className={css.imput} name = "Description" placeholder="Description*"></input>
-                    <input type="submit" value="Edit" className={css.btn} />
-                </div>
-            </form>
+            <div className={css.container}>
+                <form>
+                    <div>
+                        <button className={css.btnClose} onClick={() => { closeEditModal(false) }}>X</button>
+                        <p className={css.p}>Edit Lecture</p>
+                    </div>
+                    <div>
+                        <input className={css.imput} required = "required" name="Name" placeholder="Name*"></input>
+                        <input className={css.imput} required = "required" name="Description" placeholder="Description*"></input>
+                        <button className={css.btnCancel} onClick={() => { closeEditModal(false) }}>Cancel</button>
+                        <input type="submit" value="Edit" className={css.btnEdit} />
+                    </div>
+                </form>
+            </div>
         </section>
     )
 }
