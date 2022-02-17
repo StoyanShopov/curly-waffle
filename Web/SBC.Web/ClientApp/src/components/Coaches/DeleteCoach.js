264914d4fb6448f3a003aa9d7136965c1279c378
Daniel Hristov <danihristov1@abv.bg>
2022-02-17 16:44:24 +0200

    update-DeleteCoach-component

--- Web/SBC.Web/ClientApp/src/components/Coaches/DeleteCoach.js
+++ Web/SBC.Web/ClientApp/src/components/Coaches/DeleteCoach.js
@@ -9,6 +9,14 @@ const DeleteCoach = () => {
 
                 <div className={styles.header}>
                     <span className={styles.title}>Are you sure you want to delete this coach?</span>
+                    <button className={styles.closeBtn}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="21.92" height="21.92" viewBox="0 0 21.92 21.92">
+                            <g id="Group_46" data-name="Group 46" transform="translate(-1484.379 -241.379)">
+                                <line id="Line_59" data-name="Line 59" y2="25" transform="translate(1504.178 243.5) rotate(45)" fill="none" stroke="#fff" strokeLinecap="round" strokeWidth="3" />
+                                <line id="Line_60" data-name="Line 60" y2="25" transform="translate(1504.178 261.178) rotate(135)" fill="none" stroke="#fff" strokeLinecap="round" strokeWidth="3" />
+                            </g>
+                        </svg>
+                    </button>
                 </div>
 
                 <div className={styles.footer}>
