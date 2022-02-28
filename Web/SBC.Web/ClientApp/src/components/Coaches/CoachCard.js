452bfb58980720d21dc38987861ed2e3e51ff366
Ivan Nikolov <nikolov.iv@gmail.com>
2022-02-28 18:43:02 +0200

    improvements css

--- Web/SBC.Web/ClientApp/src/components/Coaches/CoachCard.js
+++ Web/SBC.Web/ClientApp/src/components/Coaches/CoachCard.js
@@ -3,69 +3,82 @@ import React, { useState } from "react";
 import DeleteCoach from "./DeleteCoach";
 import { Link } from "react-router-dom";
 import EditCoach from "./EditCoach";
-import Modal from 'react-modal';
+import Modal from "react-modal";
 
 const CoachCard = (props) => {
-    const [coach, setCoach] = useState(props.coach)
-    const [coaches, setCoaches] = useState(props.coaches)
+  const [coach] = useState(props.coach);
 
-    return (
-        <div className={styles.card}>
-            <div className={styles.upper}>
-                <div className={styles.pencil}>
-                    <Link 
-                    to="" 
-                    onClick={() => {props.openModal((props.editModalStyle),
-                     <EditCoach 
-                     closeModal={props.closeModal} 
-                     coach={coach} 
-                     coaches={coaches}
-                     setCoach={setCoach}
-                     setCoaches={setCoaches}
-                     languages={[coach.languages]}
-                     />)
-                      }}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 25 25">
-                            <path id="iconmonstr-pencil-2" d="M19.075,2.946l2.981,2.98L6.4,21.585l-3.732.752L3.417,18.6,19.075,2.946Zm0-2.946L1.5,17.576,0,25l7.424-1.5L25,5.926,19.075,0Z" />
-                        </svg>
-                    </Link>
-                </div>
-                <img
-                    className={styles.cardpic}
-                    src="assets/images/Mask Group 2.png" //TODO: {coach.ImageUrl}
-                    alt=""
+  return (
+    <div className={styles.card}>
+      <div className={styles.upper}>
+        <div className={styles.pencil}>
+          <Link
+            to=""
+            onClick={() => {
+              props.openModal(
+                props.editModalStyle,
+                <EditCoach
+                  closeModal={props.closeModal}
+                  coach={props.coach}
+                  coaches={props.coaches}
+                  setCoaches={props.setCoaches}
                 />
-            </div>
-            <div className={styles.down}>
-                <div className={styles.name}>
-                    <span>Management</span>
-                    <span>{coach.firstName} {coach.lastName}</span>
-                </div>
-                <div className={styles.price}>
-                    <span>{coach.pricePerSession}€ per person</span>
-                    <span>{coach.company}</span>
-                </div>
-                <div className={styles.button}>
-                    <button 
-                    onClick={() => {props.openModal(props.deleteModalStyle,
-                     <DeleteCoach 
-                     closeModal={props.closeModal} 
-                     id={coach.id} 
-                     coaches={props.coaches} 
-                     setCoaches={props.setCoaches}
-                     />)}}>Delete</button>
-                </div>
-            </div>
-            <Modal
-                style={props.subtitle}
-                isOpen={props.modalIsOpen}
-                onAfterOpen={props.afterOpenModal}
-                onRequestClose={props.closeModal}
-                ariaHideApp={false}
-            >
-                {props.child}
-            </Modal>
+              );
+            }}
+          >
+            <img src="assets/images/Group 87.svg" alt="edit" />
+          </Link>
         </div>
-    )
-}
-export default CoachCard
+        <img className={styles.cardpic} src={coach.imageUrl} alt="" />
+      </div>
+      <div className={styles.down}>
+        <div className={styles.name}>
+          <span className={styles.bold}>{coach.company.name}</span>
+          <span className={styles.names}>
+            {coach.firstName} {coach.lastName}
+          </span>
+        </div>
+        <div className={styles.price}>
+          <span className={styles.pricePerPerson}>
+            {coach.pricePerSession}€ per person
+          </span>
+          <span>
+            <img
+              className={styles.company}
+              src={coach.company.logoUrl}
+              alt="Company logo"
+            />
+          </span>
+        </div>
+        <div className={styles.buttondiv}>
+          <button
+            className={styles.button}
+            onClick={() => {
+              props.openModal(
+                props.deleteModalStyle,
+                <DeleteCoach
+                  closeModal={props.closeModal}
+                  id={coach.id}
+                  coaches={props.coaches}
+                  setCoaches={props.setCoaches}
+                />
+              );
+            }}
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+      <Modal
+        style={props.subtitle}
+        isOpen={props.modalIsOpen}
+        onAfterOpen={props.afterOpenModal}
+        onRequestClose={props.closeModal}
+        ariaHideApp={false}
+      >
+        {props.child}
+      </Modal>
+    </div>
+  );
+};
+export default CoachCard;
--- Web/SBC.Web/ClientApp/src/components/Coaches/Coaches.js
+++ Web/SBC.Web/ClientApp/src/components/Coaches/Coaches.js
@@ -1,60 +1,53 @@
 import React, { useEffect } from "react";
 import styles from "../Coaches/Coaches.module.css";
-import Modal from 'react-modal';
+import Modal from "react-modal";
 import DeleteCoach from "./DeleteCoach";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import EditCoach from "./EditCoach";
 import CoachCard from "./CoachCard";
-import { getAllCoaches } from "../../services/adminCoachesService"
+import CreateCoach from "./CreateCoach";
+import { getAllCoaches } from "../../services/adminCoachesService";
+import Skeleton from "@mui/material/Skeleton";
 
 const Coaches = () => {
-
   const deleteModalStyle = {
     content: {
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      width: '36%',
-      height: '260px',
-      bottom: 'auto',
-      marginTop: '-5%',
-      marginRight: '-50%',
-      transform: 'translate(-50%, -25%)',
-      padding: '0px',
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      width: "36%",
+      height: "260px",
+      bottom: "auto",
+      marginTop: "-5%",
+      marginRight: "-50%",
+      transform: "translate(-50%, -25%)",
+      padding: "0px",
     },
-    color: '#f00'
-  }
+    color: "#f00",
+  };
 
   const editModalStyle = {
     content: {
-      top: '70%',
-      left: '50%',
-      right: 'auto',
-      width: '38%',
-      height: '520px',
-      bottom: 'auto',
-      marginTop: '-5%',
-      marginRight: '-50%',
-      transform: 'translate(-60%, -50%)',
-      padding: '0px',
+      width:"50%",
+      margin: "0 auto",
     },
-    color: '#f00'
-  }
+    color: "#f00",
+  };
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [child, setChild] = useState();
   const [subtitle, setSubtitle] = useState();
-  const [coaches, setCoaches] = useState([])
+  const [coaches, setCoaches] = useState([]);
 
   function openModal(style, child) {
     setSubtitle(style);
-    setChild(child)
+    setChild(child);
     setModalIsOpen(true);
   }
 
   function afterOpenModal() {
-    subtitle.color = '#f00';
+    subtitle.color = "#f00";
   }
 
   function closeModal() {
@@ -62,9 +55,10 @@ const Coaches = () => {
   }
 
   useEffect(() => {
-    getAllCoaches().then(res => {
-      setCoaches(res.data)
-    })
+    getAllCoaches().then((res) => {
+      setCoaches(res.data);
+      console.log(res.data);
+    });
   }, []);
 
   return (
@@ -73,8 +67,8 @@ const Coaches = () => {
         <div className={styles.textcontainer}>
           <div></div>
           <div className={styles.innertext}>
-            <h1>Coaches</h1>
-            <p>
+            <h1 className={styles.h1}>Coaches</h1>
+            <p className={styles.p}>
               Upskill’s goal is to inspire you to master your technical and
               personal skills and give you the opportunity to gain knowledge
               from top specialists in various fields.
@@ -85,22 +79,36 @@ const Coaches = () => {
           <img
             src="assets/images/Path 3449.svg"
             alt=""
-            className={styles.background}
+            className={styles.background + styles.img}
           />
           <img src="assets/images/Group 47.svg" alt="" className={styles.guy} />
         </div>
       </div>
       <div className={styles.cardscontainer}>
-        {coaches.map(x => <CoachCard
-          key={x.id}
-          coach={x}
-          openModal={openModal}
-          closeModal={closeModal}
-          deleteModalStyle={deleteModalStyle}
-          editModalStyle={editModalStyle}
-          coaches={coaches}
-          setCoaches={setCoaches}/>)}
+        {coaches.map((x) => (
+          <CoachCard
+            key={x.id}
+            coach={x}
+            openModal={openModal}
+            closeModal={closeModal}
+            deleteModalStyle={deleteModalStyle}
+            editModalStyle={editModalStyle}
+            coaches={coaches}
+            
+          />
+        ))}
+        <div className={styles.pluscontainer}>
+          <img
+            src="assets/images/Group 79.svg"
+            alt=""
+            className={styles.plus}
+            onClick={() => {
+              openModal(editModalStyle, <CreateCoach />);
+            }}
+          />
+        </div>
       </div>
+
       <Modal
         style={subtitle}
         isOpen={modalIsOpen}
@@ -115,5 +123,3 @@ const Coaches = () => {
 };
 
 export default Coaches;
-
-
