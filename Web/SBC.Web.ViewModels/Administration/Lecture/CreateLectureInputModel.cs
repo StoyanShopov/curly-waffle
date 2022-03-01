b10ca5e2313f0d73cbfed10b8fab6974b9d8cb64
VentsislavAleksandrov <69898297+VentsislavAleksandrov@users.noreply.github.com>
2022-03-01 21:02:02 +0200

    Fix for edit and add resource modals

--- Web/SBC.Web.ViewModels/Administration/Lecture/CreateLectureInputModel.cs
+++ Web/SBC.Web.ViewModels/Administration/Lecture/CreateLectureInputModel.cs
@@ -1,13 +1,19 @@
 ﻿namespace SBC.Web.ViewModels.Administration.Lecture
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class CreateLectureInputModel
     {
+        [Required]
         public string Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public string Description { get; set; }
 
+        [Required]
         public int CourseId { get; set; }
     }
 }
--- Web/SBC.Web.ViewModels/Administration/Lecture/EditLectureInputModel.cs
+++ Web/SBC.Web.ViewModels/Administration/Lecture/EditLectureInputModel.cs
@@ -1,11 +1,16 @@
 ﻿namespace SBC.Web.ViewModels.Administration.Lecture
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class EditLectureInputModel
     {
+        [Required]
         public string Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public string Description { get; set; }
     }
 }
