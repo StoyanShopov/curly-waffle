d6f04c6a7ebe2eb2605e388fc1c97d2f74b3973d
MarielaBN <mery.nikolova@gmail.com>
2022-03-05 04:42:08 +0200

    Owner dashboard style fixed

--- Web/SBC.Web/ClientApp/src/components/ProfileOwner/Dashboard/OwnerDashboard.js
+++ Web/SBC.Web/ClientApp/src/components/ProfileOwner/Dashboard/OwnerDashboard.js
@@ -36,20 +36,26 @@ export default function OwnerDashboard() {
             <div className={styles.containerT}>
                 <table className={styles.tableContainer}>
                     <thead>
-                        <tr style={{ display: 'grid' }}>
+                        <tr className={styles.theadTr}>
                             <th className={styles.theadTh}>
-                                {/* <svg width="11.4" height="20" viewBox="0 0 11.4 20">
-                                    <path id="Path_1035" data-name="Path 1035" d="M10,108.541a1.4,1.4,0,0,1-.99-.41l-8.6-8.6a1.4,1.4,0,0,1,1.981-1.981L10,105.16l7.609-7.609a1.4,1.4,0,0,1,1.98,1.981l-8.6,8.6A1.4,1.4,0,0,1,10,108.541Z" transform="translate(108.541 -0.001) rotate(90)" fill="#fff" />
-                                </svg> */}
-                                September
-                                {/* <svg width="11.4" height="20" viewBox="0 0 11.4 20">
-                                    <path id="Path_1034" data-name="Path 1034" d="M10,0a1.4,1.4,0,0,0-.99.41L.41,9.01A1.4,1.4,0,0,0,2.391,10.99L10,3.381l7.609,7.609a1.4,1.4,0,0,0,1.98-1.981L10.99.41A1.4,1.4,0,0,0,10,0Z" transform="translate(11.4) rotate(90)" fill="#fff" />
-                                </svg> */}
+                                <td>
+                                    <svg width="11.4" height="20" viewBox="0 0 11.4 20">
+                                        <path id="Path_1035" data-name="Path 1035" d="M10,108.541a1.4,1.4,0,0,1-.99-.41l-8.6-8.6a1.4,1.4,0,0,1,1.981-1.981L10,105.16l7.609-7.609a1.4,1.4,0,0,1,1.98,1.981l-8.6,8.6A1.4,1.4,0,0,1,10,108.541Z" transform="translate(108.541 -0.001) rotate(90)" fill="#fff" />
+                                    </svg>
+                                </td>
+                                <td>September</td>
+                                <td>
+                                    <svg width="11.4" height="20" viewBox="0 0 11.4 20">
+                                        <path id="Path_1034" data-name="Path 1034" d="M10,0a1.4,1.4,0,0,0-.99.41L.41,9.01A1.4,1.4,0,0,0,2.391,10.99L10,3.381l7.609,7.609a1.4,1.4,0,0,0,1.98-1.981L10.99.41A1.4,1.4,0,0,0,10,0Z" transform="translate(11.4) rotate(90)" fill="#fff" />
+                                    </svg>
+                                </td>
                             </th>
                         </tr>
-                        {/* <svg width="926" height="4" viewBox="0 0 926 4">
-                            <line id="Line_49" data-name="Line 49" x2="925" transform="translate(0.5 0.5)" fill="none" stroke="#fff" strokeLinecap="round" strokeWidth="2" />
-                        </svg> */}
+                        {/*<tr>*/}
+                        {/*    <svg width="926" height="4" viewBox="0 0 926 4">*/}
+                        {/*        <line id="Line_49" data-name="Line 49" x2="925" transform="translate(0.5 0.5)" fill="none" stroke="#fff" strokeLinecap="round" strokeWidth="2" />*/}
+                        {/*    </svg>*/}
+                        {/*</tr>*/}
                         <tr className={styles.tableTr}>
                             <th className={styles.firstTh}>Course Name</th>
                             <th className={styles.secondTh}>Enrolled</th>
@@ -79,15 +85,19 @@ export default function OwnerDashboard() {
             <div className={styles.containerC}>
                 <table className={styles.tableContainer}>
                     <thead>
-                        <tr style={{ display: 'grid' }}>
+                        <tr className={styles.theadTr}>
                             <th className={styles.theadTh} >
-                                {/* <svg width="11.4" height="20" viewBox="0 0 11.4 20">
-                                    <path id="Path_1035" data-name="Path 1035" d="M10,108.541a1.4,1.4,0,0,1-.99-.41l-8.6-8.6a1.4,1.4,0,0,1,1.981-1.981L10,105.16l7.609-7.609a1.4,1.4,0,0,1,1.98,1.981l-8.6,8.6A1.4,1.4,0,0,1,10,108.541Z" transform="translate(108.541 -0.001) rotate(90)" fill="#fff" />
-                                </svg> */}
-                                September
-                                {/* <svg width="11.4" height="20" viewBox="0 0 11.4 20">
-                                    <path id="Path_1034" data-name="Path 1034" d="M10,0a1.4,1.4,0,0,0-.99.41L.41,9.01A1.4,1.4,0,0,0,2.391,10.99L10,3.381l7.609,7.609a1.4,1.4,0,0,0,1.98-1.981L10.99.41A1.4,1.4,0,0,0,10,0Z" transform="translate(11.4) rotate(90)" fill="#fff" />
-                                </svg> */}
+                                <td>
+                                    <svg width="11.4" height="20" viewBox="0 0 11.4 20">
+                                        <path id="Path_1035" data-name="Path 1035" d="M10,108.541a1.4,1.4,0,0,1-.99-.41l-8.6-8.6a1.4,1.4,0,0,1,1.981-1.981L10,105.16l7.609-7.609a1.4,1.4,0,0,1,1.98,1.981l-8.6,8.6A1.4,1.4,0,0,1,10,108.541Z" transform="translate(108.541 -0.001) rotate(90)" fill="#fff" />
+                                    </svg>
+                                </td>
+                                <td> September</td>
+                                <td>
+                                    <svg width="11.4" height="20" viewBox="0 0 11.4 20">
+                                        <path id="Path_1034" data-name="Path 1034" d="M10,0a1.4,1.4,0,0,0-.99.41L.41,9.01A1.4,1.4,0,0,0,2.391,10.99L10,3.381l7.609,7.609a1.4,1.4,0,0,0,1.98-1.981L10.99.41A1.4,1.4,0,0,0,10,0Z" transform="translate(11.4) rotate(90)" fill="#fff" />
+                                    </svg>
+                                </td>
                             </th>
                         </tr>
                         {/* <tr>
