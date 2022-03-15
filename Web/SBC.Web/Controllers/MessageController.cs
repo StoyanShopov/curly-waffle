3a4082456a96af356ac7887d77d3b9879d62f4ef
Marin_Iliev <m_v_iliev@abv.bg>
2022-03-15 12:59:32 +0200

    Endpoint Added

---
+++ Web/SBC.Web/Controllers/MessageController.cs
@@ -0,0 +1,19 @@
+﻿namespace SBC.Web.Controllers
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class MessageController : ApiController
+    {
+        public MessageController()
+        {
+        }
+
+        [Route(nameof(Message))]
+        public async Task<ActionResult> Message()
+        {
+            return null;
+        }
+    }
+}
