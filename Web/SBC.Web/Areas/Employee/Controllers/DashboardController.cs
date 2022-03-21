28377f65ac670e8cbba178781cab3b9adb04a119
Daniel Hristov <danihristov1@abv.bg>
2022-03-21 18:24:06 +0200

    Add GetSessionsAsync Endpoint

---
+++ Web/SBC.Web/Areas/Employee/Controllers/DashboardController.cs
@@ -0,0 +1,26 @@
+﻿namespace SBC.Web.Areas.Employee.Controllers
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+    using SBC.Services.Data.Employees;
+    using SBC.Services.Data.Infrastructures;
+
+    public class DashboardController : EmployeesBaseController
+    {
+        private readonly IEmployeesDashboardService employeesDashboardService;
+
+        public DashboardController(IEmployeesDashboardService employeesDashboardService)
+        {
+            this.employeesDashboardService = employeesDashboardService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetSessionsAsync()
+        {
+            var result = await this.employeesDashboardService.GetUserCoacheSessionsAsync(this.User.Id());
+
+            return this.GenericResponse(result);
+        }
+    }
+}
