343acc8e603f9e5bf611512c30c10f7f1d640070
Daniel Hristov <danihristov1@abv.bg>
2022-04-05 16:08:19 +0300

    add token const

--- Web/SBC.Web/ClientApp/src/services/coach-service.js
+++ Web/SBC.Web/ClientApp/src/services/coach-service.js
@@ -1,11 +1,14 @@
 import axios from 'axios';
 import { baseUrl } from '../constants';
+import { TokenManagement } from '../helpers';
+
+const token = TokenManagement.getLocalAccessToken()
 
 const create = async (data) => {
     try {
         const resp = await axios.post(baseUrl + "Administration/Coaches", data, {
             headers: {
-                Authorization: `Bearer ${localStorage.getItem('token')}`,
+                Authorization: `Bearer ${token}`,
             },
         });
         return resp;
@@ -16,7 +19,7 @@ const update = async (data) => {
     try {
         const resp = await axios.put(baseUrl + `Administration/Coaches/${data.id}`, data, {
             headers: {
-                Authorization: `Bearer ${localStorage.getItem('token')}`,
+                Authorization: `Bearer ${token}`,
             },
         });
         return resp;
@@ -26,7 +29,7 @@ const update = async (data) => {
 const getAll = async () =>{
         return await axios.get(baseUrl + "Administration/Coaches", {
             headers: {
-                Authorization: `Bearer ${localStorage.getItem('token')}`,
+                Authorization: `Bearer ${token}`,
             }
         })
 }
@@ -34,7 +37,7 @@ const getAll = async () =>{
 const deleteCoach = async (coachId) =>{
     const resp = await axios.delete(baseUrl + `Administration/Coaches/${coachId}` , {
         headers: {
-            Authorization: `Bearer ${localStorage.getItem('token')}`,
+            Authorization: `Bearer ${token}`,
         }
     }).then((resp) => {
     })
